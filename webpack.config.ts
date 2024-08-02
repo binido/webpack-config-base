8d080c4adb8a0152bd8ef07f8adf90c845641dcb
bnd <zename9@gmail.com>
2024-08-02 22:45:36 +0400

    first build

---
+++ webpack.config.ts
@@ -0,0 +1,64 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+  mode: Mode;
+  port: number;
+}
+
+export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development';
+
+  const config: webpack.Configuration = {
+    mode: env.mode ?? 'development',
+    entry: {
+      main: path.resolve(__dirname, 'src', 'index.ts'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      isDev ? new webpack.ProgressPlugin() : undefined,
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
+  };
+  return config;
+};
